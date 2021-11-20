9ee3999df10993995ecc07fd331510998006ddd7
ndkv9 <vu.nguyen101@gmail.com>
2021-11-20 18:00:26 +0200

    fetch data using useEffect hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import api from '../lib/api'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
-
-const fetchData = async () => {
-  const result = await api.getUsersDiff()
-  console.log(result)
-}
+import InfoTable from './InfoTable'
 
 export const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await api.getUsersDiff()
+        console.log(result)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchData()
+  }, [])
   return (
     <Container className='app' fixed>
       <Box data-testid='app-box' m={2}>
         <Typography>Your app should show up here.</Typography>
         {/* Just a dummy fetcher to show how the api should be used, this should be removed */}
-        <Button variant='contained' color='primary' onClick={fetchData}>
+        <Button variant='contained' color='primary'>
           Test data fetch
         </Button>
+        <InfoTable />
       </Box>
     </Container>
   )
