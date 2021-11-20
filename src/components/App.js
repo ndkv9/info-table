e883bcdd247a3a5dd3a9d71307b8189ad8f27264
ndkv9 <vu.nguyen101@gmail.com>
2021-11-20 20:17:05 +0200

    display loading state

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,11 @@ import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
 import InfoTable from './InfoTable'
+import { Typography } from '@material-ui/core'
 
 export const App = () => {
   const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     fetchData()
@@ -14,12 +16,15 @@ export const App = () => {
   console.log('data', data)
 
   const fetchData = async () => {
+    setIsLoading(true)
     try {
       const result = await api.getUsersDiff()
       setData(prev => [...prev, ...result.data])
     } catch (error) {
       console.log(error)
     }
+
+    setIsLoading(false)
   }
 
   return (
@@ -27,9 +32,13 @@ export const App = () => {
       <Box data-testid='app-box' m={2}>
         <InfoTable data={data} />
 
-        <Button variant='contained' color='primary' onClick={fetchData}>
-          Load more
-        </Button>
+        {isLoading ? (
+          <Typography>Loading..</Typography>
+        ) : (
+          <Button variant='contained' color='primary' onClick={fetchData}>
+            Load more
+          </Button>
+        )}
       </Box>
     </Container>
   )
