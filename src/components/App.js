a09ad37e632644fc517dcf1955ba2c2aae1b0d7f
ndkv9 <vu.nguyen101@gmail.com>
2021-11-20 19:22:41 +0200

    use fetching data separate on a button

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import api from '../lib/api'
 import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
 import InfoTable from './InfoTable'
@@ -10,31 +9,27 @@ export const App = () => {
   const [data, setData] = useState([])
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await api.getUsersDiff()
-        setData(prev => [...prev, ...result.data])
-      } catch (error) {
-        console.log(error)
-      }
-    }
     fetchData()
   }, [])
   console.log('data', data)
 
+  const fetchData = async () => {
+    try {
+      const result = await api.getUsersDiff()
+      setData(prev => [...prev, ...result.data])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <Container className='app' fixed>
       <Box data-testid='app-box' m={2}>
-        <Typography>Your app should show up here.</Typography>
-        {/* Just a dummy fetcher to show how the api should be used, this should be removed */}
-        <Button
-          variant='contained'
-          color='primary'
-          onClick={() => console.log('clicked!')}
-        >
+        <InfoTable data={data} />
+
+        <Button variant='contained' color='primary' onClick={fetchData}>
           Load more
         </Button>
-        <InfoTable data={data} />
       </Box>
     </Container>
   )
--- src/components/InfoTable.js
+++ src/components/InfoTable.js
@@ -14,18 +14,6 @@ const useStyles = makeStyles({
   },
 })
 
-// function createData(name, calories, fat, carbs, protein) {
-//   return { name, calories, fat, carbs, protein }
-// }
-
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24),
-//   createData('Ice cream sandwich', 237, 9.0, 37),
-//   createData('Eclair', 262, 16.0, 24),
-//   createData('Cupcake', 305, 3.7, 67),
-//   createData('Gingerbread', 356, 16.0, 49),
-// ]
-
 const InfoTable = ({ data }) => {
   const classes = useStyles()
 
