6dc9d4a3261155c6d05e4747acaba006496ec7ca
ndkv9 <vu.nguyen101@gmail.com>
2021-11-23 20:38:19 +0200

    change table title to Typography

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import api from '../lib/api'
 import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
 import InfoTable from './InfoTable'
+import Typography from '@material-ui/core/Typography'
 
 export const App = () => {
   const { getUsersDiff, getProjectsDiff } = api
@@ -10,12 +11,12 @@ export const App = () => {
     <Container className='app' fixed>
       <Box data-testid='app-box' m={2}>
         <React.Fragment>
-          <h2>Users History</h2>
+          <Typography variant='h4'>Users History</Typography>
           <InfoTable getData={getUsersDiff} />
         </React.Fragment>
 
         <React.Fragment>
-          <h2>Projects History</h2>
+          <Typography variant='h4'>Projects History</Typography>
           <InfoTable getData={getProjectsDiff} />
         </React.Fragment>
       </Box>
