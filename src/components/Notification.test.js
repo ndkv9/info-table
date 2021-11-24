2318f59e3a2de0a23f8053408a249b55f922377c
ndkv9 <vu.nguyen101@gmail.com>
2021-11-24 23:10:40 +0200

    add test cases for Notification

--- src/components/Notification.test.js
+++ src/components/Notification.test.js
@@ -4,16 +4,29 @@ import Notification from './Notification'
 
 describe('<Notification />', () => {
   let wrapper
-  let message = 'I am a message'
-
-  beforeEach(() => {
-    wrapper = shallow(<Notification notification={message} />)
-  })
 
   describe('render()', () => {
-    it('renders the proper message', () => {
+    it('do not display as default', () => {
+      wrapper = shallow(<Notification isError={false} isFetchedAll={false} />)
+      expect(wrapper.find({ 'data-testid': 'notification' }).exists()).toBe(
+        false,
+      )
+    })
+
+    it('display when having error with proper message', () => {
+      wrapper = shallow(<Notification isError={true} isFetchedAll={false} />)
       expect(wrapper.find({ 'data-testid': 'notification' })).toHaveLength(1)
-      expect(wrapper.text()).toEqual('I am a message')
+      expect(wrapper.find({ 'data-testid': 'notification' }).text()).toBe(
+        'We had problems fetching your data. Please try again!',
+      )
+    })
+
+    it('display when fetching all data with proper message', () => {
+      wrapper = shallow(<Notification isError={false} isFetchedAll={true} />)
+      expect(wrapper.find({ 'data-testid': 'notification' })).toHaveLength(1)
+      expect(wrapper.find({ 'data-testid': 'notification' }).text()).toBe(
+        'Fetched all data!',
+      )
     })
   })
 })
